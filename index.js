eeb7f78cd1372abaccf640f44d16f5908725c5f5
karis dalton <58391690+karisdalton@users.noreply.github.com>
2020-12-01 12:54:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ let i;
 
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+//     toggle adding and removing .active class
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
