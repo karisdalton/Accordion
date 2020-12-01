47eb20bdbf589a6386da4f02c36e04ab5f54c413
Dalton <daltonkaris2000@gmail.com>
2020-12-01 12:51:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let accordion = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
